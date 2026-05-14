0f8a5327e8df547ea696447010e097679207ef73
Suchit Peddoju <K2118297@students.katyisd.org>
2026-05-13 19:39:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-function sayHi() {
-  alert("It works! Your Netlify site is running 🚀");
+let score = 0;
+let time = 10;
+let gameRunning = false;
+let timer;
+
+function startGame() {
+  score = 0;
+  time = 10;
+  gameRunning = true;
+
+  document.getElementById("score").innerText = "Score: 0";
+  document.getElementById("time").innerText = "Time: 10";
+
+  timer = setInterval(() => {
+    time--;
+    document.getElementById("time").innerText = "Time: " + time;
+
+    if (time === 0) {
+      clearInterval(timer);
+      gameRunning = false;
+      alert("Time's up! Your score: " + score);
+    }
+  }, 1000);
+}
+
+function clickMe() {
+  if (!gameRunning) return;
+
+  score++;
+  document.getElementById("score").innerText = "Score: " + score;
 }
